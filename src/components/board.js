7d96ca9afdc0ef0cc10f959ba958fed4fb559717
AndreLT <terencio.andre2@gmail.com>
2020-10-22 22:34:05 -0300

    Automatic step functionality and manual cell input

--- src/components/board.js
+++ src/components/board.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { render } from 'react-dom';
 
 import Canvas from './canvas'
@@ -10,8 +10,12 @@ let toLive = new Map();
 let toDie = new Map();
 
 const Board = () => {
-    
+
     const [generation, setGeneration] = useState(0);
+    const [intervalId, setIntervalId] = useState(false);
+    const [size, setSize] = useState(10);
+    const [canvas, setCanvas] = useState(<Canvas onClick={e => addManually(e)} generation={generation} alive={alive} size={size}/>);
+    const [fps, setFps] = useState(300);
 
 
     const rNeighbours = (element, testdead) => {
@@ -22,12 +26,50 @@ const Board = () => {
                 if(alive.has(i + '-' + j)){
                     neighbours++;
                 }
-                else if(testdead && rNeighbours({coord:{x: i, y: j}}, false) == 2){
+                else if(testdead && rNeighbours({coord:{x: i, y: j}}, false) === 2){
                     toLive.set(i + '-' + j,{x:i, y:j})
                 }
             }
         }
         return neighbours -1;
+    }
+
+    const drawBoard = () => {
+        console.log("drew board");
+    }
+
+    const addManually = (e) => {
+
+        let relativeW = e.target.offsetLeft;
+        let relativeH = e.target.offsetTop;
+
+        let x = Math.floor((e.clientX - relativeW)/size)
+        let y = Math.floor((e.clientY - relativeH)/size)
+
+        
+        
+        if(intervalId)
+            toLive.set(x + '-' + y,{x: x, y: y})
+        else{
+            alive.set(x + '-' + y, {coord:{x:x, y:y}})
+            updateCanvas();
+        }
+    }
+
+    const runtime = (speed) => {
+        const intId = window.setInterval(() => step(), speed);
+        setIntervalId(intId);
+    }
+
+    const stop = () => {
+        clearInterval(intervalId);
+        setIntervalId(false);
+    }
+
+    const speed = (news) => {
+        stop();
+        runtime(news);
+        setFps(news);
     }
 
     const createModel = (model) => {
@@ -36,6 +78,7 @@ const Board = () => {
             toLive.set(model[i].x + '-' + model[i].y,model[i])
         }
         eConceive();
+        updateCanvas();
     } 
 
     const eConceive = () => {
@@ -46,7 +89,6 @@ const Board = () => {
     }
 
     const eKill = () => {
-        console.log(toDie)
         for (let [key, value] of toDie) {
             alive.delete(key)
         }
@@ -54,33 +96,46 @@ const Board = () => {
     }
 
     const step = () => {
-
+        
         for (let [key, value] of alive) {
             let neighbours = rNeighbours(value, true);
 
             if(neighbours > 3 || neighbours < 2)
                 toDie.set(key, value);
         };
+        setGeneration(generation+1)
         eConceive();
         eKill();
-        setGeneration(generation+1);
+        updateCanvas();
     }
 
     const start = () => {
         setGeneration(1);
     }
+
+    const updateCanvas = () => {
+        setCanvas(<Canvas onClick={e => addManually(e)} generation={generation} alive={alive} size={size}/>)
+    }
+      
         
-    return <>
+    return <div class="main">
         <text>Generation: {generation}</text>
-        {alive.size ? 
-            <Canvas alive={alive} size={10}/>
-            : null
-        }
-        <button onClick={()=> start()}>Start</button>
-        <button onClick={()=> step()}>Step</button>
-        <button onClick={()=> createModel(gun)}>Glider Gun</button>
-        <button onClick={()=> createModel(glider)}>Glider</button>
-    </>
+        {canvas}
+        <div class="button-group">
+            <button onClick={()=> start()}>Start</button>
+            <button onClick={()=> step()}>Step</button>
+            <button onClick={()=> createModel(gun)}>Glider Gun</button>
+            <button onClick={()=> createModel(glider)}>Glider</button>
+            <button onClick={()=> drawBoard()}>Clear Board</button>
+            <button onClick={()=> runtime(fps)} disabled={intervalId}>Run Simulation</button>
+            <button onClick={()=> stop()}>Stop Simulation</button>
+        </div>
+        <div>
+            <text>Speed Control</text>
+            <button onClick={()=> speed(fps+50)} disabled={fps >= 2000}>-</button>
+            <button onClick={()=> speed(fps-50)} disabled={fps <= 50}>+</button>
+        </div>
+    </div>
 }
 
 export default Board;
--- src/components/canvas.js
+++ src/components/canvas.js
@@ -1,35 +1,43 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 
-const Canvas = (props) => {
+const Canvas = props => {
   const canvasRef = useRef(null)
+  const cellSelected = {x:0, y:0};
+  const [ctx, setCtx] = useState(null);
+  let relativeLocation = null;
 
-  const draw = (ctx) => {
+  const draw = () => {
+    
     for (let [key, value] of props.alive) {
       const x = value.coord.x*props.size;
       const y = value.coord.y*props.size;
 
       ctx.beginPath();
-      ctx.rect(x, y, props.size, props.size);
+      ctx.rect(x, y, props.size-1, props.size-1);
       ctx.fill();
     }
   }
-  
+
   useEffect(() => {
     const canvas = canvasRef.current;
-    const context = canvas.getContext('2d');
+    const context = canvas.getContext('2d')
+    setCtx(context);
 
-    context.canvas.width=window.screen.width
-    context.canvas.height=window.screen.height
+    //getting canvas width
+    let width=Math.floor(window.innerWidth*0.7)
+    let height=Math.floor(window.innerHeight*0.9)
 
-    draw(context)
+    context.canvas.width=width
+    context.canvas.height=height
+    console.log(width, height)
+
+    relativeLocation=({x:context.canvas.offsetLeft, y:context.canvas.offsetTop})
+
+    draw()
   })
 
   
-  return <>
-    <div class="view">
-      <canvas className="canvas" ref={canvasRef} {...props}/>
-    </div>
-  </>
+  return <canvas class="view" ref={canvasRef} {...props}/>
 }
 
 export default Canvas
